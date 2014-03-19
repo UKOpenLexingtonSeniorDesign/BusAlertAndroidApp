f03728f5025b6df1bac01179c419584c5eaecd7f
adhe223 <adhe223@g.uky.edu>
2014-03-19 19:08:57 -0400

    Added URL Grabber
    -Grabs the stops fata from lextran URL in alert.java
    -Need to make sure teh AsyncTask sets the value in the main thread.
    
    To Do Next:
    -Parse the xml
    -Populate the Spinners based on the stop values

--- Android/BusAlert/src/io/github/ukopenlexingtonseniordesign/busalert/Alert.java
+++ Android/BusAlert/src/io/github/ukopenlexingtonseniordesign/busalert/Alert.java
@@ -1,13 +1,78 @@
 package io.github.ukopenlexingtonseniordesign.busalert;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import android.R.string;
 import android.app.Activity;
+import android.content.Context;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 public class Alert extends Activity{
+	private String XMLToParse;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.alert);
+		
+		XMLTask task = new XMLTask();
+	    task.execute(new Integer[] { 10 });		//Get stops for route 10
 	}
+	
+	private class XMLTask extends AsyncTask<Integer, String, String> {
+        String xml = null;
+        String url = "http://realtime.lextran.com/InfoPoint/map/GetRouteXml.ashx?RouteID=";
+		
+	    @Override
+	    protected void onPreExecute() {
+	        // Do stuff
+	        // For example showing a Dialog to give some feedback to the user.
+	    }
+
+	    @Override
+	    protected void onPostExecute(String xml) {
+	        // If you have created a Dialog, here is the place to dismiss it.
+	        // The `xml` that you returned will be passed to this method
+	    	XMLToParse = xml;
+	    }
+
+		@Override
+		protected String doInBackground(Integer... stops) {
+			
+			url = url + stops[0];
+	        try {	        	
+	            // defaultHttpClient
+	            DefaultHttpClient httpClient = new DefaultHttpClient();
+	            HttpPost httpPost = new HttpPost(url);
+	 
+	            HttpResponse httpResponse = httpClient.execute(httpPost);
+	            HttpEntity httpEntity = httpResponse.getEntity();
+	            xml = EntityUtils.toString(httpEntity);
+	 
+	        } catch (UnsupportedEncodingException e) {
+	            e.printStackTrace();
+	        } catch (ClientProtocolException e) {
+	            e.printStackTrace();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+		
+	        //return xml
+	        return xml;
+		}
+	}	       
 }
+
+
